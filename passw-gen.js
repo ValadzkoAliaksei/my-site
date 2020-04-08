eac526c00d9e0e8cb84e1d9db364ec8a2c93d662
Valadzkoaliaksei <Valadzkoaliaksei@tut.by>
2020-04-08 15:36:38 +0300

    Красивая структура кода

--- passw-gen.js
+++ passw-gen.js
@@ -1,32 +1,32 @@
 
 
-var letters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-function readInt(){
+function readInt() {
 	var number = document.getElementById("userAnswer").value;
 	return parseInt(number);
 	// return +document.getElementById("userAnswer").value;
 }
 
-function write(text){
+function write(text) {
 	document.getElementById("passw").innerHTML = text;
 }
 
-function getRandomNumber(max){
+function getRandomNumber(max) {
 	return Math.round(Math.random() * max)
 }
 
-function generateText(letters, length){
+function generateText(letters, length) {
 	var text = "";
-	for(var i = 0; i < length; i++){
+	for (var i = 0; i < length; i++) {
 		var n = getRandomNumber(letters.length - 1);
 		text = text + letters[n];
 	}
-	return(text);
+	return (text);
 }
 
-function generate(){
-	var length=readInt();
+function generate() {
+	var length = readInt();
 	var passw = generateText(letters, length);
 	write(passw);
 }
--- riddle.js
+++ riddle.js
@@ -1,25 +1,25 @@
 var score = 0;
 
-		function checkAnswer(inputId, answers){
-			var userAnswer = document.getElementById(inputId).value;
-			userAnswer = userAnswer.toLowerCase();
-			for(var i = 0; i < answers.length; i++){
-				if(userAnswer == answers[i]){
-					score++;
-					break;
-				}
-			}
+function checkAnswer(inputId, answers) {
+	var userAnswer = document.getElementById(inputId).value;
+	userAnswer = userAnswer.toLowerCase();
+	for (var i = 0; i < answers.length; i++) {
+		if (userAnswer == answers[i]) {
+			score++;
+			break;
 		}
+	}
+}
 
-		function write(text){
-			document.getElementById("info").innerHTML = text;
-		}
+function write(text) {
+	document.getElementById("info").innerHTML = text;
+}
 
-		function checkAnswers() {
+function checkAnswers() {
 
-			checkAnswer("userAnswer1", ["сон", "сноведение"]);
-			checkAnswer("userAnswer2", ["морской", "укус акулы"]);
-			checkAnswer("userAnswer3", ["шахматный", "мертвый"]);
+	checkAnswer("userAnswer1", ["сон", "сноведение"]);
+	checkAnswer("userAnswer2", ["морской", "укус акулы"]);
+	checkAnswer("userAnswer3", ["шахматный", "мертвый"]);
 
-			write("<b>Вы отгадали " + score + " загадок</b>");
-		}
+	write("<b>Вы отгадали " + score + " загадок</b>");
+}
